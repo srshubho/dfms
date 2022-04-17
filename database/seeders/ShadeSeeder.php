bcbbd7200d8d5d5e4b8b941e89a5e3f134aecc17
Muhammad Shihab Rahman (shubho) <coder.srs@gmail.com>
2022-04-17 13:54:49 +0000

    commit from gitpod

--- database/seeders/ShadeSeeder.php
+++ database/seeders/ShadeSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Shade;
+use File;
 
 class ShadeSeeder extends Seeder
 {
@@ -14,5 +16,18 @@ class ShadeSeeder extends Seeder
     public function run()
     {
         //
+        Shade::truncate();
+  
+        $json = File::get("database/data/shade.json");
+        $shades = json_decode($json);
+  
+        foreach ($shades as $key => $value) {
+            Country::create([
+                "shade_no" => $value->name,
+                "shade_area" => $value->shade_area,
+                "shade_capacity" => $value->shade_capacity,
+                "shade_type" => $value->shade_type
+            ]);
+        }
     }
 }
