f619ccf1be8cdda5849d9f096e631e2e4b5dd723
shihab rahman <coder.srs@gmail.com>
2022-09-06 16:35:41 +0600

    changes in CowSeeder class

--- database/seeders/CowSeeder.php
+++ database/seeders/CowSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Cow;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Seeder;
 
 class CowSeeder extends Seeder
@@ -14,6 +15,61 @@ class CowSeeder extends Seeder
      */
     public function run()
     {
-        Cow::factory()->count(5)->create();
+        
+        // Cow::factory()->count(5)->create();
+        $cow = [
+            [
+                'name' => "madhuri",
+                'date_of_purchased' => date("Y-m-d"),
+                'transition_cost' => 1000,
+                'labour_cost' => 500,
+                'estimated_live_weight' => 550,
+                'is_purchased' => 1
+            ],
+        
+            [
+                'name' => "mousumi",
+                'date_of_purchased' => date("Y-m-d",strtotime("2022-4-23")),
+                'transition_cost' => 1000,
+                'labour_cost' => 500,
+                'estimated_live_weight' => 550,
+                'is_purchased' => 1
+            ],
+        
+            [
+                'name' => "lila",
+                'date_of_purchased' => date("Y-m-d",strtotime("2021-4-23")),
+                'transition_cost' => 1000,
+                'labour_cost' => 500,
+                'estimated_live_weight' => 500,
+                'is_purchased' => 1
+            ],
+        
+            [
+                'name' => "lali",
+                'date_of_purchased' => date("Y-m-d",strtotime("2022-4-23")),
+                'transition_cost' => 1000,
+                'labour_cost' => 500,
+                'estimated_live_weight' => 650,
+                'is_purchased' => 1
+            ],
+        
+            [
+                'name' => "koli",
+                'date_of_purchased' => date("Y-m-d",strtotime("2022-4-23")),
+                'transition_cost' => 1000,
+                'labour_cost' => 500,
+                'estimated_live_weight' => 550,
+                'is_purchased' => 1
+            ],
+        
+
+       
+        ];
+
+        foreach ($cow as $cow) {
+            Cow::create($cow);
+        }
     }
-}
+    }
+
